22e8bfb64deee7b283eb1dbcdfb529661c97e159
thetitasghosh <rg814602@gmail.com>
2025-04-14 20:11:53 +0530

    update user buuton

--- src/components/App/Buttons/user-button.jsx
+++ src/components/App/Buttons/user-button.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+"use client";
+import React, { use, useEffect, useState } from "react";
 import { Button } from "../../ui/button";
+import { getUserData } from "@/app/actions";
+import { getEmailInitial } from "@/lib/utils";
 const UserButton = () => {
+  const [name, setName] = useState("");
+  useEffect(() => {
+    const fetch = async () => {
+      const user = await getUserData();
+      setName(user.email);
+    };
+    fetch();
+  }, []);
+
   return (
     <Button variant={""} size={"icon"}>
-      BI
+      {getEmailInitial(name)}
     </Button>
   );
 };
